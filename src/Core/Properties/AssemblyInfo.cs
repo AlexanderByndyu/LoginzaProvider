932ce5c9a07745ed50d61e6229e739ff38ebe9e9
Alexander Byndyu <alexander.byndyu@gmail.com>
2013-05-26 21:29:49 +0600

    Выставил версию 0.0.2

--- src/Core/Properties/AssemblyInfo.cs
+++ src/Core/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a4e026f7-9a94-4371-b094-ffcf6cb7c435")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
